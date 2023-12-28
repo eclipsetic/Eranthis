d71fbcfdba8e0c39dd2fff6d15bfffc513bd98a8
eclipsetic <109125435+eclipsetic@users.noreply.github.com>
2023-12-28 08:57:50 +0600

    +

--- Fenolic.R
+++ Fenolic.R
@@ -5,3 +5,12 @@ library(pacman)
 pacman::p_load(readxl, ggplot2)
 setwd("E:/Eranthis")
 fenolic <- read_excel("fenolic.xlsx")
+
+ggplot(fenolic) +
+  geom_bar( aes(x=name, y=leaves), stat="identity", fill="skyblue", alpha=0.7)
+
+data <- data.frame(
+  name=letters[1:5],
+  value=sample(seq(4,15),5),
+  sd=c(1,0.2,3,2,4)
+)
