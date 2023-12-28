51d77494e699ccd006510a59d172cccbef9c5e3e
eclipsetic <109125435+eclipsetic@users.noreply.github.com>
2023-12-28 12:40:42 +0600

    +

--- Fenolic.R
+++ Fenolic.R
@@ -2,15 +2,31 @@
 Sys.setenv(LANG = "en_US.UTF-8")
 #Library
 library(pacman)
-pacman::p_load(readxl, ggplot2)
+pacman::p_load(readxl, ggplot2, scales)
 setwd("E:/Eranthis")
 fenolic <- read_excel("fenolic.xlsx")
 
-ggplot(fenolic) +
-  geom_bar( aes(x=name, y=leaves), stat="identity", fill="skyblue", alpha=0.7)
+fenolic$leaves <- as.numeric(as.character(fenolic$leaves))
+fenolic$flowers <- as.numeric(as.character(fenolic$flowers))
 
-data <- data.frame(
-  name=letters[1:5],
-  value=sample(seq(4,15),5),
-  sd=c(1,0.2,3,2,4)
-)
+fenolic_long <- tidyr::gather(fenolic, key = "variable", value = "value", -name)
+
+# Создание графика с разделением по оси x
+ggplot(fenolic_long, aes(x = name, y = value, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Leaves and Flowers Comparison", x = "Name", y = "Count") +
+  scale_fill_manual(values = c("leaves" = "green", "flowers" = "red")) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_grid(. ~ variable, scales = "free_x", space = "free_x") +
+  scale_y_continuous(labels = comma)
+
+ggplot(fenolic_long, aes(x = name, y = value, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Leaves and Flowers Comparison", x = "Name", y = "Count") +
+  scale_fill_manual(values = c("leaves" = "green", "flowers" = "red")) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_y_continuous(labels = comma, breaks = seq(0, max(fenolic_long$value), by = 50000000))
+
+
