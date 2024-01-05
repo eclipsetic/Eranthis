baa84cd6cd22b988a42ccc80bda89cf5c4116d87
eclipsetic <109125435+eclipsetic@users.noreply.github.com>
2024-01-05 20:48:34 +0600

    +

--- Fenolic.R
+++ Fenolic.R
@@ -2,7 +2,7 @@
 Sys.setenv(LANG = "en_US.UTF-8")
 #Library
 library(pacman)
-pacman::p_load(readxl, ggplot2, scales)
+pacman::p_load(readxl, ggplot2, scales, extrafont)
 setwd("E:/Eranthis")
 fenolic <- read_excel("fenolic.xlsx")
 
@@ -23,10 +23,33 @@ ggplot(fenolic_long, aes(x = name, y = value, fill = variable)) +
 
 ggplot(fenolic_long, aes(x = name, y = value, fill = variable)) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Leaves and Flowers Comparison", x = "Name", y = "Count") +
+  labs(x = "Peak area, ion count", y = "Сompound") +
   scale_fill_manual(values = c("leaves" = "green", "flowers" = "red")) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   scale_y_continuous(labels = comma, breaks = seq(0, max(fenolic_long$value), by = 50000000))
 
+ggplot(fenolic_long, aes(x = factor(name, levels = unique(name)), y = value, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Peak area, ion count", y = "Сompound") +
+  scale_fill_manual(values = c("leaves" = "green", "flowers" = "red")) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_y_continuous(labels = comma, breaks = seq(0, max(fenolic_long$value), by = 50000000))
 
+loadfonts(device = "win", quiet = TRUE)
+font <- "Palatino Linotype"
+
+ggplot(fenolic_long, aes(x = factor(name, levels = unique(name)), y = value, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Сompound", y = "Peak area, ion count") +
+  scale_fill_manual(values = c("leaves" = "green", "flowers" = "red")) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, family = font),
+        text = element_text(family = font),
+        axis.title = element_text(family = font),
+        plot.title = element_text(family = font),
+        legend.text = element_text(family = font),
+        legend.title = element_text(family = font),
+        legend.position = "right") +
+  scale_y_continuous(labels = comma, breaks = seq(0, max(fenolic_long$value), by = 50000000))
