02e2a82c3a2463c26f9fb7f50ac434c249df1612
eclipsetic <109125435+eclipsetic@users.noreply.github.com>
2024-01-29 03:16:35 +0600

    +

--- Analysis.R
+++ Analysis.R
@@ -2,31 +2,9 @@
 Sys.setenv(LANG = "en_US.UTF-8")
 #Library
 library(pacman)
-pacman::p_load(
-  lubridate,
-  purrr,
-  dplyr,
-  tidyr,
-  forecast,
-  zoo,
-  rlang,
-  ggplot2,
-  tidyverse,
-  raster,
-  sp,
-  geodata,
-  terra,
-  rasterVis,
-  BiocManager,
-  devtools,
-  dismo,
-  XML,
-  jsonlite,
-  rgdal,
-  rJava,
-  readxl,
-  rgbif,
-  leaflet
+pacman::p_load(lubridate, purrr, dplyr, tidyr, forecast, zoo, rlang, ggplot2, tidyverse, raster,
+  sp, geodata, terra, rasterVis, BiocManager, devtools, dismo, XML, jsonlite, rgdal, rJava,
+  readxl, rgbif, leaflet, factoextra, NbClust, cluster, Hmisc, caret, mice, missForest
 )
 
 
@@ -99,3 +77,52 @@ for (species_name in species_list) {
     print(paste("Error: latitude or longitude column not found for species", species_name))
   }
 }
+
+
+# Analysis --------------------------------------------------------------------------------------------------------
+
+#Making one table from all
+
+all_dataframes <- list("E.sibirica", "E.tanhoensis", "E.sibirica_x_E.tanhoensis", "E.krasnoborovii", "E.sineli",
+                       "E.stellata", "E.stellata.Korea.", "E.pinnatifida", "E.pungdoensis", "E.albiflora",
+                       "E.lobulata", "E.byunsanensis")
+
+tables <- lapply(all_dataframes, get)
+tables <- lapply(tables, function(tbl) mutate_all(tbl, as.character))
+combined_data <- bind_rows(tables)
+
+# Claster
+
+data_for_clustering <- combined_data %>% dplyr::select(Species, PHfl)
+
+data_for_clustering <- na.omit(data_for_clustering)
+data_for_clustering$PHfl <- as.numeric(data_for_clustering$PHfl)
+# Create a distance matrix
+dist_matrix <- dist(imputed_data)
+
+# Perform hierarchical clustering
+hc <- hclust(dist_matrix, method = "ward.D2")
+
+# Plot the dendrogram
+plot(hc, main = "Hierarchical Clustering Dendrogram", sub = NULL, xlab = NULL, cex = 0.8)
+
+
+
+
+# Выберем только нужные столбцы для кластеризации
+data_for_clustering <- combined_data %>% dplyr::select(Species, PHfl)
+data_for_clustering$PHfl <- as.numeric(data_for_clustering$PHfl)
+data_for_clustering <- data_for_clustering[complete.cases(data_for_clustering), ]
+data_for_clustering$PHfl <- as.numeric(as.character(data_for_clustering$PHfl), na.rm = TRUE)
+# Кластеризация методом k-средних (k-means)
+kmeans_result <- kmeans(data_for_clustering, centers = 3)
+
+# Визуализация результатов
+plot(data_for_clustering, col = kmeans_result$cluster, main = "K-means Clustering")
+
+# Вывод информации о кластерах
+cat("Cluster Centers:\n")
+print(kmeans_result$centers)
+
+# Вывод результатов кластеризации
+combined_data$cluster <- kmeans_result$cluster
